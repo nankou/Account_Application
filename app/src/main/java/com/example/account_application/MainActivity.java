17e47aff9ec09876d0747897a0d07afc98a36eb3
aaaji <ajing__chen@163.com>
2021-05-19 10:29:02 +0800

    tabLayout依赖

--- app/src/main/java/com/example/account_application/MainActivity.java
+++ app/src/main/java/com/example/account_application/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.account_application;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
